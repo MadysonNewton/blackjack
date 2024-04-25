b348482a69c2f29199d8551afec7cdee65a64dbc
Madyson Newton <mnewton.prsvr@gmail.com>
2024-04-24 20:03:00 -0500

    first commit

---
+++ notes.js
@@ -0,0 +1,74 @@
+// let errorParagraph = document.getElementById("error")
+// console.log(errorParagraph)
+// errorParagraph.textContent = " Something went wrong, please try again"
+
+// function purchase() {
+//     console.log("button clicked")
+// }
+
+let num1 = 8
+let num2 = 2
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+
+// Create 4 fuctions: add(), subtract(), divide(), multiply()
+function add(){
+console.log("add clicked")
+}
+function subtract() {
+
+}
+
+function divide() {
+
+}
+function multiply() {
+
+}
+//call the correct function when the user clicks on one of the buttons
+//perform the given calculation using num1 and num2
+//render the result of the calculation in the paragraph with id="sum-el"
+
+//E.g. if the user clicks on the "plus" button, you should render
+//"sum: 10" (since 8+2=10) inside the paragraph with id="sum-el"
+
+
+
+
+
+
+
+
+
+
+
+// let myPoints = 3
+//creat two functions, add3Points() and remove1Points(), and have them add/remove points to/from the myPoints variable
+
+
+
+// function  add3Points() {
+// myPoints += 3
+// }
+
+
+// function remove1Points() {
+//     myPoints -= 1
+// }
+
+// add3Points()
+// add3Points()
+// add3Points()
+// remove1Points()
+// remove1Points()
+
+//call the function to that the line below logs out 10
+// console.log(myPoints)
+
+// try to predict what each of the lines will log out
+// console.log("2" + 2) 22
+// console.log(11 + 7) 18
+// console.log(6 + "5") 65
+// console.log("My Points: " + 5 + 9) My Points: 59
+// console.log(2 + 2) 4
+// console.log("11" + "14") 1114
