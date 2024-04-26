c4026fdafab09962dcaf4dd0d2a502ee6d400921
Madyson Newton <mnewton.prsvr@gmail.com>
2024-04-25 19:56:40 -0500

    started project

--- notes.js
+++ notes.js
@@ -6,25 +6,29 @@
 //     console.log("button clicked")
 // }
 
-let num1 = 8
-let num2 = 2
-document.getElementById("num1-el").textContent = num1
-document.getElementById("num2-el").textContent = num2
+// let num1 = 8
+// let num2 = 2
+// document.getElementById("num1-el")
+// document.getElementById("num2-el")
 
-// Create 4 fuctions: add(), subtract(), divide(), multiply()
-function add(){
-console.log("add clicked")
-}
-function subtract() {
+// // Create 4 fuctions: add(), subtract(), divide(), multiply()
+// function add() {
+//     let result = num1 + num2
+//   sumEl.textContent + "sum: " + num1 + num2
+// }
+// function subtract() {
+//     let result = num1 - num2
+//     sumEl.textContent = "sum: " + num1 + num2
+// }
 
-}
-
-function divide() {
-
-}
-function multiply() {
-
-}
+// function divide() {
+//     let result = num1 / num2
+//     sumEl.textContent = "sum: " + num1 + num2
+// }
+// function multiply() {
+//     let result = num1 * num2
+//     sumEl.textContent = "sum: " + num1 + num2
+// }
 //call the correct function when the user clicks on one of the buttons
 //perform the given calculation using num1 and num2
 //render the result of the calculation in the paragraph with id="sum-el"
@@ -32,7 +36,22 @@ function multiply() {
 //E.g. if the user clicks on the "plus" button, you should render
 //"sum: 10" (since 8+2=10) inside the paragraph with id="sum-el"
 
+// let age = 22
+// if (age <= 20) {
+//     console.log("You cannot enter the club!")
+// }else {
+//     console.log("Welcome!")
+// }
 
+// let age = 100
+
+// if (age < 100) {
+//     console.log("Not elegible")
+// } else if (age === 100) {
+//     console.log("Here is your birthday card from the King!")
+// } else {
+//     console.log("Not elegible, you have already gotten one.")
+// }
 
 
 
---
+++ script.js
@@ -0,0 +1,14 @@
+let firstCard = 9
+let secondCard = 9
+let sum = firstCard + secondCard
+
+console.log(sum)
+//conditions are what go into the ()
+if (sum <= 20) {
+    console.log("Do you want to draw a new card?😊")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack!🥳")
+} else {
+    console.log("You're out of the game!😭")
+}
+
