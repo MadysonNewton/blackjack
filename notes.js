603abf78151f0f9fed7c527c4af8fe4a43a4b588
Madyson Newton <mnewton.prsvr@gmail.com>
2024-05-07 19:48:33 -0500

    notes and worked on project

--- notes.js
+++ notes.js
@@ -53,18 +53,44 @@
 //     console.log("Not elegible, you have already gotten one.")
 // }
 
-console.log(4 === 3)
-console.log(5 > 2)
-console.log(12 > 12)
-console.log(3 < 0)
-console.log(3 >= 3)
-console.log(11 <= 11)
-console.log(3 <= 2)
+// console.log(4 === 3)
+// console.log(5 > 2)
+// console.log(12 > 12)
+// console.log(3 < 0)
+// console.log(3 >= 3)
+// console.log(11 <= 11)
+// console.log(3 <= 2)
 
 
 
+// Arrays are an ordered lists of items
+// 0 indexed
+// let featuredPosts = ["Check out my Netflix clone", 
+// "Here's the code for my project", 
+// " I've just relauched my portfolio"]
+// my practice array down below
+// let myPosts = ["Check out my Cat Painting", 
+// "Here is my certificate for completing the project", 
+// "I have uploaded it to indeed for others to see"]
 
+//Make the following appear in the console
+// console.log(experience[1])
+// console.log(experience[2])
+// console.log(experience[0])
 
+// let cards = [7, 4]
+// cards.push(6)
+// console.log(cards)
+// let messages = [
+//     "Hey, how's it going?",
+//     "I'm great, thank you! How about you?",
+//     "All good. Been working on my portfolio lately."
+// ]
+// let newMessage = "Same here!"
+// messages.push(newMessage)
+// console.log(messages)
+// messages.pop()
+// console.log(messages)
 
 
 // let myPoints = 3
@@ -97,3 +123,88 @@ console.log(3 <= 2)
 // console.log("My Points: " + 5 + 9) My Points: 59
 // console.log(2 + 2) 4
 // console.log("11" + "14") 1114
+
+
+
+// ++ is an increment operator
+// += It is a shorthand notation for adding a value to a variable and assigning the result back to the variable
+// its called the addition assignment operator
+// it can also be -=
+// subtraction
+
+// Count to ten!
+// We need to specify
+
+// WHere should we START counting?
+// Where is the FINISHline?
+// What's the STEP SIZE we should use?
+
+//        start           finish        step size 
+// for (let count = 1; count < 11; count +=1 ) {
+//     console.log(count)
+// }
+
+// create a for loop that counts from 10 to 100 in steps of 10
+// use console.log to log out the numbers.
+
+// for (let i = 10; i < 101; i +=10) {
+//     console.log(i)
+// }
+
+// let cards = [7, 3, 9]
+
+// create a for loop that logs out all the cards in the Array.
+// Use cards.length to specify how long the loop should run
+
+// for (let card = 0; card < cards.length; card += 1)  {
+    
+//     console.log(cards[i])
+// }
+
+
+// let sentence = ["Hello ", "my ", "name ", "is ", "Mady"]
+// let greetingEl = document.getElementById("greeting-el")
+
+//render the sentence in the greetingEl paragraph using a for loop and .textContent
+
+// for (let i = 0; i < sentence.length; i++) {
+//     greetingEl.textContent += sentence[i]
+// }
+
+// let player1Time = 102
+// let player2Time = 107
+// let totalRaceTime = ""
+
+// function getFastestRaceTime(){
+//     if (player1Time < player2Time) {
+//         return player1Time
+//     } else if(player2Time < player1Time) {
+//         return player2Time
+//     } else {
+//         return player1Time
+//     }
+// }
+
+// let fastestRace = getFastestRaceTime()
+
+// let fastestRace = getFastestRaceTime() {
+//     console.log(fastestRaceTime)
+// }
+
+// write a function that returns the total race time
+// call/invoke the function and store the returned value in a new variable
+// finally, log the variable out
+
+// function getTotalRaceTime() {
+// return player1Time + player2Time
+// }
+
+// let totalTime = getTotalRaceTime()
+
+// console.log(totalTime)
+
+// let randomNumber = Math.random()
+
+// console.log(randomNumber)
+// Returns a pseudorandom number between 0 and 1.
+// let randomNumber = Math.random()
--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-let firstCard = 3
-let secondCard = 5
-let sum = firstCard + secondCard
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards = [firstCard, secondCard] //array - ordered list of items
+let sum = firstCard + secondCard 
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
-function startGame(){
+
+
+
+
+// create a function, getRandomCard(), that always returns the number 5
+// use getRandomCard() to set the value of the firstCard and secondCard
+
+function getRandomCard() {
+return 5
+}
+
+function startGame() {
+    renderGame()
+}
+
+
+function renderGame(){
+    cardsEl.textContent = "cards "
+
     if(sum <= 20){
         message = "Would you like to draw again?"
     }
@@ -21,8 +40,17 @@ function startGame(){
     }
     messageEl.textContent=message
     sumEl.textContent= "Sum: "+sum
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    //render all the cards we have
 }
-function newGame(){
+function newCard(){
     console.log("Drawing a new Card from the deck!")
+    
+   let card = getRandomCard() 
+   sum += card
+   renderGame()
+   cards.push(Card)
+   console.log(card)
+   
 }
+
